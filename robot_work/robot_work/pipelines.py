904c246f57a60655f38032603c418300473d66f1
Zhangluo <643733581@qq.com>
2024-11-11 17:43:31 +0800

    :bookmark: ci: 构建镜像

--- robot_work/robot_work/pipelines.py
+++ robot_work/robot_work/pipelines.py
@@ -3,8 +3,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import logging
-from idlelib.iomenu import errors
-
+from pymongo import errors
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from pymongo import MongoClient
--- robot_work/robot_work/push_initial_urls.py
+++ robot_work/robot_work/push_initial_urls.py
@@ -3,4 +3,4 @@ import redis
 r = redis.Redis(host='jzhangluo.com', port=6379, db=0, password='qwertyuiop353680509.')
 for offset in range(0, 1715, 35):
     url = f'https://music.163.com/discover/playlist/?cat=华语&order=hot&limit=35&offset={offset}'
-    r.lpush('list_url_queue', url)
+    r.lpush('test', url)
